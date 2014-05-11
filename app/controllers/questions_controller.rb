f91f9fc83cb35f0b1ce0c46d7b89d2ed5839076e
drewbgenter <drewbgenter@gmail.com>
2014-05-11 13:40:23 -0500

    questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,23 @@ class QuestionsController < ApplicationController
     # What is the most recent movie on the list that the second actor appeared in?
 
     # Your Ruby goes here.
+    the_actor = Actor.second
+
+    the_actors_movies = []
+
+    the_actor.roles.each do |the_role|
+      the_actors_movies.push(the_role.movie)
+    end
+
+    most_recent_movie = { :year => 0, :title => "" }
+
+    the_actors_movies.each do |the_movie|
+      if the_movie.year.to_i > most_recent_movie[:year]
+        most_recent_movie = { :year => the_movie.year.to_i, :title => the_movie.title }
+      end
+    end
+
+    @most_recent_movie_for_second_actor = most_recent_movie[:title]
 
     # @most_recent_movie_for_second_actor = ???
   end
@@ -12,13 +29,22 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    # @director_of_longest_movie = ???
+    @director_of_longest_movie = Movie.order("duration DESC").first.director.name
   end
 
   def question_3
     # Which director has the most movies on the list?
 
     # Your Ruby goes here.
+    movie_counts = []
+
+    Director.all.each do |the_director|
+      number_of_movies_directed_by_the_director = the_director.movies.count
+
+      movie_counts.push(number_of_movies_directed_by_the_director)
+    end
+
+    @most_number_of_movies_by_a_single_director = movie_counts.max
 
     # @director_with_the_most_movies = ???
   end
